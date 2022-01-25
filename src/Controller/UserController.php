0e40ed45e9cd0d185fee0637352f24011788bd62
ujikstark <ujikbauk@gmail.com>
2022-01-25 10:51:46 +0700

    add user controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'user')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        $data = [];
+        for ($i = 0; $i < count($users); $i++) {
+            $data[] = [
+                'id' => $users[$i]->getId(),
+                'email' => $users[$i]->getEmail()
+            ];
+        }
+
+        return $this->json([
+
+            'data' => $data
+        ]);
+    }
+}
