25820c095e376cd1ea41b33f0cc0b8f8020798c4
ujikstark <ujikbauk@gmail.com>
2022-03-02 16:36:11 +0700

    install refresh-token

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
--- tests/Functional/Entity/TodoTest.php
+++ tests/Functional/Entity/TodoTest.php
@@ -35,28 +35,123 @@ class TodoTest extends AbstractEndPoint
             Request::METHOD_POST,
             self::TODOS_URI,
             $this->getPayload(),
-            [],
-            true,
-            ''
+        );
+
+        $content = $response->getContent();
+        $contentDecoded = json_decode($content, true);
+        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertJson($content);
+        $this->assertArrayHasKey('date', $contentDecoded);
+        return (string) $contentDecoded['id'];
+
+    }
+
+    /**
+     * @depends testPostTodo
+     */
+    public function testPutTodo(string $id): void
+    {
+        $response = $this->getResponseFromRequest(
+            Request::METHOD_PUT,
+            self::TODOS_URI.'/'.$id,
+            $this->getUpdatePayload(),
         );
 
         $content = $response->getContent();
         $contentDecoded = json_decode($content, true);
 
-        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
-
-        return (string) $contentDecoded['id'];
-
-
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($content);
+        $this->assertArrayHasKey('date', $contentDecoded);
     }
+
+    /**
+     * @depends testPostTodo
+     */
+    public function testDeleteTodo(string $id): void
+    {
+        $response = $this->getResponseFromRequest(
+            Request::METHOD_DELETE,
+            self::TODOS_URI.'/'.$id
+        );
+
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $response->getStatusCode());
+    }
+
+    public function testGetTodosNotLoggedIn(): void
+    {
+        $response = $this->getResponseFromRequest(
+            Request::METHOD_GET,
+            self::TODOS_URI,
+            '',
+            [],
+            false
+        );
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testPostTodoNotLoggedIn(): void
+    {
+        $response = $this->getResponseFromRequest(
+            Request::METHOD_GET,
+            self::TODOS_URI,
+            $this->getPayload(),
+            [],
+            false
+        );
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testPutTodoNotLoggedIn(): void
+    {
+        $response = $this->getResponseFromRequest(
+            Request::METHOD_PUT,
+            self::TODOS_URI.'/1',
+            $this->getPayload(),
+            [],
+            false
+        );
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testDeleteTodoNotLoggedIn(): void
+    {
+        $response = $this->getResponseFromRequest(
+            Request::METHOD_DELETE,
+            self::TODOS_URI.'/1',
+            $this->getPayload(),
+            [],
+            false
+        );
+
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
 
     private function getPayload(): string
     {
         $faker = Factory::create();
         $name = $faker->word();
         $description = $faker->sentence();
-        $date = (new \DateTime())->getTimestamp() * 1000;
+        $date = new DateTime();
+        $date->modify('+5 day');
+        $dateFormatted = $date->format('Y-m-d H:i');
 
-        return sprintf('{"name":"%s","description":"%s","isDone":false}', $name, $description);
+        return sprintf('{"name":"%s","description":"%s","date":"%s","isDone":false}', $name, $description, $dateFormatted);
+    }
+
+    private function getUpdatePayload(): string
+    {
+        $faker = Factory::create();
+        $name = $faker->word();
+        $description = $faker->sentence();
+        $date = new DateTime();
+        $date->modify('+5 day');
+        $dateFormatted = $date->format('Y-m-d H:i');
+
+        return sprintf('{"name":"%s","description":"%s","date":"%s","isDone":false}', $name, $description, $dateFormatted);
     }
 }
