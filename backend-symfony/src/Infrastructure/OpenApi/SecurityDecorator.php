89ba62519151849a0c8d5988117e33703561e522
ujikstark <ujikbauk@gmail.com>
2022-03-14 10:08:12 +0700

    updated login docs swagger

--- backend-symfony/src/Infrastructure/OpenApi/SecurityDecorator.php
+++ backend-symfony/src/Infrastructure/OpenApi/SecurityDecorator.php
@@ -31,20 +31,6 @@ final class SecurityDecorator implements OpenApiFactoryInterface
 
     private function createLoginPath(?\ArrayObject $schemas): PathItem
     {
-        $schemas['Token'] = new \ArrayObject([
-            'type' => 'object',
-            'properties' => [
-                'token' => [
-                    'type' => 'string',
-                    'readOnly' => true,
-                ],
-                'refresh_token' => [
-                    'type' => 'string',
-                    'readOnly' => true,
-                ]
-            ],
-        ]);
-
         $schemas['Credentials'] = new \ArrayObject([
             'type' => 'object',
             'properties' => [
@@ -65,14 +51,10 @@ final class SecurityDecorator implements OpenApiFactoryInterface
                 operationId: 'login',
                 tags: ['Security'],
                 responses: [
-                    Response::HTTP_OK => [
+                    Response::HTTP_NO_CONTENT => [
                         'description' => 'Authentication successful',
                         'content' => [
-                            'application/json' => [
-                                'schema' => [
-                                    '$ref' => '#/components/schemas/Token',
-                                ],
-                            ],
+                            'application/json' => [],
                         ],
                     ],
                     Response::HTTP_UNAUTHORIZED => [
