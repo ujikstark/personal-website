c4b94e9a5fa98e41ed0a3ce4da585a18eed65e6d
ujikstark <ujikbauk@gmail.com>
2022-03-15 15:19:05 +0700

    handle add button

--- frontend-react/src/components/TodoForm.js
+++ frontend-react/src/components/TodoForm.js
@@ -1,36 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Col, Row, Button } from "react-bootstrap";
 
-function TodoForm () {
+function TodoForm ({ todos, setTodos, todo }) {
+
+    const [newTodo, setNewTodo] = useState({});
+    
+   
+    const handleSubmit =  (e) => {
+        e.preventDefault();
+       
+        setTodos({
+            ...todos,
+            newTodo
+        })
+        
+    }
+
+    const handleChange = e => {
+        let { name, value } = e.target;
+        setNewTodo({
+            [name]: value
+        });
+    }
+
     return (
-        <Form as={Row}>
-            <Form.Group className="mb-4" as={Col} md={6} >
-                <Form.Label>Task<span className="ml-1 text-danger">*</span></Form.Label>
-                <Form.Control
-                    id="name" name="name" type="text" placeholder="A task"/>
-                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-4" as={Col} md={6} >
-                <Form.Label>Description</Form.Label>
-                <Form.Control
-                    id="description" name="description" type="text" placeholder="A Description"/>
-                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-5" as={Col} sm={6} >
-                <Form.Label>Date</Form.Label>
-                <Form.Control
-                    id="date" name="date" type="datetime-local"/>
-                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-5" as={Col} sm={6} >
-                <Form.Label>Reminder</Form.Label>
-                <Form.Control
-                    id="reminder" name="reminder" type="datetime-local"/>
-                <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
-            </Form.Group>
-            <Col>
-                <Button>Add Todo</Button>
-            </Col>
+
+        <Form onSubmit={handleSubmit}>
+            <Row>
+                <Form.Group className="mb-4" as={Col} md={6} >
+                    <Form.Label>Task<span className="ml-1 text-danger">*</span></Form.Label>
+                    <Form.Control
+                        onChange={handleChange} value={newTodo.name} id="name" name="name" type="text" placeholder="A task" />
+                    <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-4" as={Col} md={6} >
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control
+                        onChange={handleChange} value={newTodo.description} id="description" name="description" type="text" placeholder="A Description"/>
+                    <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-5" as={Col} sm={6} >
+                    <Form.Label>Date</Form.Label>
+                    <Form.Control
+                        onChange={handleChange} value={newTodo.date} id="date" name="date" type="datetime-local"/>
+                    <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-5" as={Col} sm={6} >
+                    <Form.Label>Reminder</Form.Label>
+                    <Form.Control
+                        onChange={handleChange} value={newTodo.reminder} id="reminder" name="reminder" type="datetime-local"/>
+                    <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                </Form.Group>
+                <Col>
+                    <Button type="submit">Add Todo</Button>
+                </Col>
+            </Row>
         </Form>
     )
 }
--- frontend-react/src/components/TodoTable.js
+++ frontend-react/src/components/TodoTable.js
@@ -1,8 +1,16 @@
 import React from "react"
 import TodoForm from "./TodoForm";
 
-function TodoTable() {
-    return <TodoForm/>
+function TodoTable({ todos, setTodos }) {
+
+    if (!Object.keys(todos).length) {
+        return <TodoForm todos={todos} setTodos={setTodos} todo={{}}/>
+    }
+
+    return <h1>wow ada 1</h1>
+
+    
+
 }   
 
 export default TodoTable;
