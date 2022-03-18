d1004a5512d7ddbaf1e99666f75564deab95c46b
ujikstark <ujikbauk@gmail.com>
2022-03-18 14:03:01 +0700

    done button with api

--- frontend-react/src/components/TodoTable/DoneButton.js
+++ frontend-react/src/components/TodoTable/DoneButton.js
@@ -1,20 +1,23 @@
 import { Button, Col } from "react-bootstrap";
+import { useAuth, useAuthUpdate } from "../../contexts/AuthContext";
 import { editTodo } from "../../requests/todos";
 
 export default function DoneButton ({ todo, todos, setTodos }) {
-    
-    const handleEdit = (todo) => {
-        todo.isDone = !todo.isDone;
 
-        const newTodos = editTodo(todo, todos);
-        
+    const auth = useAuth();
+    const updateAuth = useAuthUpdate();
+    
+    const handleEdit = async (todo) => {
+        todo.isDone = !todo.isDone;
+        const newTodos = await editTodo(todo, todos, auth, updateAuth);
+
         if (newTodos === todos) {
             return;
-        }
+        }   
 
         setTodos(newTodos);
-    }
 
+    };
     return (
         <Col className="d-flex justify-content-center align-items-center pr-1 pl-1" xs={1} sm={1}>
             <Button className="rounded-circle p-2" size="lg" variant={todo.isDone === true ? 'dark' : 'primary'} onClick={() => handleEdit(todo)}>
