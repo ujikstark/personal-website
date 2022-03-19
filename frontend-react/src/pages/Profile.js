e6efa95a8dca456d64852fce143c0d843f86f14b
ujikstark <ujikbauk@gmail.com>
2022-03-19 10:52:18 +0700

    add update password modal

---
+++ frontend-react/src/pages/Profile.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from 'react-router-dom';
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Helmet } from "react-helmet";
+import UpdatePasswordModal from '../components/UpdatePasswordModal';
+
+function Profile () {
+    const auth = useAuth();
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        if (auth === null) {
+            navigate("/", { replace: true });
+        }
+    }, [auth]);
+
+
+    return (
+        <Container>
+            <Helmet><title>My Profile</title></Helmet>
+            <Card className="m-2 p-3 shadow">
+                <Card.Body>
+                    <Card.Title className="mb-4">Account's setttings</Card.Title>
+                    <UpdatePasswordModal/>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
+}
+
+export default Profile;
