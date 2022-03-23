9cd58951c974a20a01e7b5de7a121e7892188758
ujikstark <ujikbauk@gmail.com>
2022-03-23 10:24:58 +0700

    update repository with entity manager

--- backend-symfony/src/App/Repository/UserRepository.php
+++ backend-symfony/src/App/Repository/UserRepository.php
@@ -15,25 +15,13 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class UserRepository extends ServiceEntityRepository 
 {
+    use EntityManagerTrait;
+    
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
-    }
-
-    /**
-     * Used to upgrade (rehash) the user's password automatically over time.
-     */
-    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
-    {
-        if (!$user instanceof User) {
-            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
-        }
-
-        $user->setPassword($newHashedPassword);
-        $this->_em->persist($user);
-        $this->_em->flush();
     }
 
     // /**
