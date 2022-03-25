03a2de9fb0f3362c61916f4c95d4a61cac5ee8d8
ujikstark <ujikbauk@gmail.com>
2022-03-25 10:05:01 +0700

    install mercure

--- backend-symfony/config/bundles.php
+++ backend-symfony/config/bundles.php
@@ -12,4 +12,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
