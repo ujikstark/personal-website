92567523a00f43c1e55df6df0ceb53654ec5b616
ujikstark <ujikbauk@gmail.com>
2022-03-26 16:00:08 +0700

    update serializer message entity

--- backend-symfony/src/App/Entity/Message.php
+++ backend-symfony/src/App/Entity/Message.php
@@ -45,16 +45,34 @@ class Message
 
     #[ORM\ManyToOne(targetEntity: Participant::class, inversedBy: 'messages')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Serializer\Groups(groups: [
+        Conversation::READ_COLLECTION_GROUP,
+        Conversation::READ_ITEM_GROUP,
+        Message::CREATE_GROUP,
+    ])]
     private Participant $sender;
 
     #[ORM\ManyToOne(targetEntity: Conversation::class, inversedBy: 'messages')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Serializer\Groups(groups: [
+        Message::CREATE_GROUP,
+    ])]
     private Conversation $conversation;
     
     #[ORM\Column(type: 'text')]
+    #[Serializer\Groups(groups: [
+        Conversation::READ_COLLECTION_GROUP,
+        Conversation::READ_ITEM_GROUP,
+        Message::CREATE_GROUP,
+    ])]
     private string $content = '';
 
     #[ORM\Column]
+    #[Serializer\Groups(groups: [
+        Conversation::READ_COLLECTION_GROUP,
+        Conversation::READ_ITEM_GROUP,
+        Message::CREATE_GROUP,
+    ])]
     private \DateTimeImmutable $date;
 
     public function __construct()
